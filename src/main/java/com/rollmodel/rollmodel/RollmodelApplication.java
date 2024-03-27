b56ffc15fe32cdb82687ba7c48ad00e441ad0552
kyle-heller <kyle.heller@gmail.com>
2024-03-26 23:02:20 -0400

    Added spring boot initializer files

---
+++ src/main/java/com/rollmodel/rollmodel/RollmodelApplication.java
@@ -0,0 +1,13 @@
+package com.rollmodel.rollmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollmodelApplication.class, args);
+	}
+
+}
